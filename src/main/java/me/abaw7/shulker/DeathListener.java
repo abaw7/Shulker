3468afe42cc8225c34cc1431f4e7f6340eae97d1
Abaw7 <abaw7@users.noreply.github.com>
2020-04-18 15:55:37 -0700

    Removed extraneous logging
    Signed-off-by: Abaw7 <abaw7@users.noreply.github.com>

--- src/main/java/me/abaw7/shulker/DeathListener.java
+++ src/main/java/me/abaw7/shulker/DeathListener.java
@@ -23,10 +23,8 @@ public class DeathListener implements Listener {
     @EventHandler
     public void onShulkerDeath(EntityDeathEvent event) {
         if (event.getEntity() instanceof Shulker) {
-            logger.info("Shulker Death");
             Shulker shulker = (Shulker) event.getEntity();
             if (shulker.getEntitySpawnReason() != CreatureSpawnEvent.SpawnReason.CUSTOM) {
-                logger.info("Setting drops to 2 shells");
                 event.getDrops().clear();
                 event.getDrops().add(new ItemStack(Material.SHULKER_SHELL, 2));
             }
